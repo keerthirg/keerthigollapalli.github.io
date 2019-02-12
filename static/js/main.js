6a21d740533a1875872d45c7bdb48cadf686e788
gsahadevan <gaus06techian@gmail.com>
2019-02-12 12:58:23 +0100

    added code for tum mock test

---
+++ static/js/main.js
@@ -0,0 +1,91 @@
+function loadFile() {
+
+    $('#dynamiccontent').empty();
+        
+    var selectObj = document.getElementById('chaptername');
+    var filename = selectObj.options[selectObj.selectedIndex].value;
+
+    readTextFile("/static/data/" + filename, function(resp) {
+        var firstObj = JSON.parse(resp);
+        // console.log(firstObj);
+        var ctr = 0;
+        var htmlText = '';
+
+        var shuffleQuestions = document.getElementById('shuffle_questions').checked;
+
+        var jsonObj = firstObj;
+
+        if (shuffleQuestions) {
+            jsonObj = shuffle(firstObj);
+        }
+
+        jsonObj.forEach(function(obj) {
+            // console.log(obj.solution);
+            ctr++;
+            htmlText += '<br>';
+            htmlText += '<fieldset>';
+            htmlText += '<legend> Question ' + ctr + ' of ' + jsonObj.length + ' </legend>';
+            htmlText += '<p class="question">' + obj.question + '</p>';
+            var loopVar = 0;
+
+            var shuffleOptions = document.getElementById('shuffle_options').checked;
+
+            var jsonOptions = obj.options;
+
+            if (shuffleOptions) {
+                jsonOptions = shuffle(obj.options);    
+            }
+            
+            jsonOptions.forEach(function(option) {
+                loopVar++;
+                var opt_text = option.substring(2);
+                if (obj.solution.includes(option.substring(0, 2).trim())) {
+                    htmlText += '<br><input type="radio" id="' + ctr + '_' + loopVar + '" name="' + ctr + '">';
+                    htmlText += '<div class="' + ctr + '" style="display: inline;"><label class="option"> ' + opt_text + '</label></div>';
+                } else {
+                    htmlText += '<br><input type="radio" id="' + ctr + '_' + loopVar + '" name="' + ctr + '"><label class="option"> ' + opt_text + '</label>';
+                    htmlText += '<div class="' + ctr + '" style="display: none;"></div>';
+                }
+            })
+            htmlText += '</fieldset>';
+        });
+        // $('body').append(htmlText);
+        $('#dynamiccontent').append(htmlText);
+        $(document).ready(function() {
+            $('input[type=radio]').on('change', function(event) {
+                var test = event.target.id.split("_", 2);
+                $("." + test[0]).css("color", "green");
+                $("." + test[0]).css("border", "solid 2px");
+                $("." + test[0]).css("font-weight", "bold");
+            });
+        });
+    });
+}
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
---
+++ test.md
@@ -0,0 +1,35 @@
+---
+layout: page
+title: Mock Test
+permalink: /test/
+---
+
+<form id="jsonFile" name="jsonFile" enctype="multipart/form-data" method="post">
+<div id="selector">
+    <div id="toggles">
+        <label class="checkbox-inline">
+            <input type="checkbox" id="shuffle_questions" checked data-toggle="toggle"> Shuffle Questions
+        </label>
+        &nbsp;
+        <label class="checkbox-inline">
+            <input type="checkbox" id="shuffle_options" checked data-toggle="toggle"> Shuffle Options
+        </label>
+    </div>
+    <br>
+    Select a Test (and click Load) : 
+    <select id="chaptername">
+        <option value="bookkeeping-chap1.json">Bookkeeping - Chapter 1</option>
+        <option value="bookkeeping-chap2.json">Bookkeeping - Chapter 2</option>
+        <option value="bookkeeping-chap3.json">Bookkeeping - Chapter 3</option>
+        <option value="bookkeeping-chap4.json">Bookkeeping - Chapter 4</option>
+        <option value="bookkeeping-chap5.json">Bookkeeping - Chapter 5</option>
+        <option value="bookkeeping-chap6.json">Bookkeeping - Chapter 6</option>
+        <option value="bookkeeping-chap7.json">Bookkeeping - Chapter 7</option>
+        <option value="bookkeeping-chap8.json">Bookkeeping - Chapter 8</option>
+    </select>
+    &nbsp;
+    <input type='button' id='btnLoad' value='Load' onclick='loadFile();'>
+</div>
+<div id="dynamiccontent"></div>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>    
+<script type="text/javascript" src="/static/js/main.js"></script>
