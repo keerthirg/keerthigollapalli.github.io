356053986eeeb57bfc7441b4c04f30fe25f9687f
gsahadevan <gaus06techian@gmail.com>
2019-02-13 05:20:29 +0100

    added logic for correct attempts

--- _site/static/js/main.js
+++ _site/static/js/main.js
@@ -1,13 +1,19 @@
+var q_obj = [];
+var s_obj = [];
+
 function loadFile() {
 
     $('#dynamiccontent').empty();
+    $('#test-progress').empty();
         
     var selectObj = document.getElementById('chaptername');
     var filename = selectObj.options[selectObj.selectedIndex].value;
 
     readTextFile("/static/data/" + filename, function(resp) {
+
         var firstObj = JSON.parse(resp);
         // console.log(firstObj);
+
         var ctr = 0;
         var htmlText = '';
 
@@ -22,8 +28,12 @@ function loadFile() {
         jsonObj.forEach(function(obj) {
             // console.log(obj.solution);
             ctr++;
+
+            // adding the values to q_obj
+            q_obj[ctr] = obj.solution[0];
+
             htmlText += '<br>';
-            htmlText += '<fieldset class="f_container">';
+            htmlText += '<fieldset class="f_container"><div class="d_' + ctr + '">';
             htmlText += '<legend> Question ' + ctr + ' of ' + jsonObj.length + ' </legend>';
             htmlText += '<p class="question">' + obj.question + '</p>';
             var loopVar = 0;
@@ -39,19 +49,20 @@ function loadFile() {
             jsonOptions.forEach(function(option) {
                 loopVar++;
                 var opt_text = option.substring(2);
+                var val = option.substring(0, 2).trim();
                 if (obj.solution.includes(option.substring(0, 2).trim())) {
                     htmlText += '<div class="options_container">';
                     htmlText += '<div class="div_' + ctr + '" id="' + ctr + '_crt' + '"><label class="option">';
-                    htmlText += '<input type="radio" id="' + ctr + '_' + loopVar + '" name="' + ctr + '"> ' + opt_text + '</label></div>';
+                    htmlText += '<input type="radio" id="' + ctr + '_' + loopVar + '" name="' + ctr + '" value="' + val + '"> ' + opt_text + '</label></div>';
                     htmlText += '</div>';
                 } else {
                     htmlText += '<div class="options_container">';
                     htmlText += '<div class="div_' + ctr + '" id="opt_' + ctr + '_' + loopVar + '"><label class="option">';
-                    htmlText += '<input type="radio" id="' + ctr + '_' + loopVar + '" name="' + ctr + '"> ' + opt_text + '</label></div>';
+                    htmlText += '<input type="radio" id="' + ctr + '_' + loopVar + '" name="' + ctr + '" value="' + val + '"> ' + opt_text + '</label></div>';
                     htmlText += '</div>';
                 }
             })
-            htmlText += '</fieldset>';
+            htmlText += '</div></fieldset>';
         });
         // $('body').append(htmlText);
         $('#dynamiccontent').append(htmlText);
@@ -65,6 +76,30 @@ function loadFile() {
                 }
                 $("#" + test[0] + "_crt").css("color", "green");
                 $("#" + test[0] + "_crt").css("border", "solid 2px");
+
+                $('.d_' + test[0] + ' input:radio').attr("disabled", true);
+
+                // adding values to s_obj
+                var value = $("#" + event.target.id).val();
+                s_obj[test[0]] = value;
+                // console.log(value);
+                // console.log(q_obj);
+                // console.log(s_obj);
+                var attempt = s_obj.length - 1;
+                var total = q_obj.length - 1;
+                var success = 0;
+                for (var key in s_obj) {
+                    // console.log(s_obj[key] + "  " + q_obj[key]);
+                    if (s_obj[key] === q_obj[key]) {
+                        success++;
+                    }
+                }
+                $('#test-progress').empty();
+                var html_progress = '';
+                html_progress += '<span class="total">Total Questions : ' + total + ' </span>';
+                html_progress += '<span class="attempted">Attempted : ' + attempt + ' </span>';
+                html_progress += '<span class="success">Correct : ' + success + ' </span>';
+                $('#test-progress').append(html_progress);
             });
         });
     });
